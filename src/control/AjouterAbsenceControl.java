34ba244891226b779ad437bbc1b578bf48e5910e
ErwanAgnelet <erwan.agnelet@bts-malraux.net>
2016-05-04 15:29:14 +0200

    Ajout des controleurs
    closed #25 #26 #27 #28

---
+++ src/control/AjouterAbsenceControl.java
@@ -0,0 +1,15 @@
+package control;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AjouterAbsenceControl implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
---
+++ src/control/VisualiserRetardControl.java
@@ -0,0 +1,14 @@
+package control;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VisualiserRetardControl implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
